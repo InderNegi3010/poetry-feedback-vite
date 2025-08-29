f7dc383d175c2a7f43bdc818eb36aae474b9c7ff
Inder Singh Negi <snegi1070@gmail.com>
2025-08-29 15:10:54 +0530

    Fix syntax error in AnalysisGrid

--- src/components/poetry/AnalysisGrid.jsx
+++ src/components/poetry/AnalysisGrid.jsx
@@ -42,7 +42,6 @@ export default function AnalysisGrid({ analysis, hasErrors }) {
                       {section.syllables.map((syllable, syllableIdx) => {
                         const isError = typeof syllable === 'object' && syllable.isError;
                         const syllableText = typeof syllable === 'object' ? syllable.text : syllable;
-                        const cellColor = isError ? 'bg-red-200' : section.color;
                         
                         return (
                           <td
